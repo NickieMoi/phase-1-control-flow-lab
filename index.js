74fce27f2d0388766f9ac9ba0cd391703390622a
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-07 16:12:30 +0300

    commit tree 2a40684a275d0daf0a04b9c893fcdb20900c0238

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return 'This one is on me!'
+  } else if (1999 < feet && feet < 2500){
+      return "I will gladly take your thirty bucks."
+  } else {(feet > 2500)
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(City){
+  
+  return City ==='NYC'?'Ok, sounds good.':'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.'
+        break;
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.'
+      default:
+        return 'Bye.'
+    }
+  }
